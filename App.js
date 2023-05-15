da5e496643a23cc3684e013419bfbfb21881f700
Khulon <e0725788@u.nus.edu>
2023-05-15 09:38:24 +0800

    eclaim test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working fon your apsdsdp!</Text>
+      <Text>Eclaim!</Text>
       <StatusBar style="auto" />
     </View>
   );
